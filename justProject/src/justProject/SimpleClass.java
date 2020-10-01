52e32146e9a996d418dd55875458391bd64789fe
1 <1@192.168.1.16>
2020-10-01 01:28:35 -0700

    added few words

--- justProject/src/justProject/SimpleClass.java
+++ justProject/src/justProject/SimpleClass.java
@@ -2,7 +2,7 @@ package justProject;
 
 public class SimpleClass {
 	public static void main(String[] args) {
-		System.out.println("Hello, What's up?");
+		System.out.println("Hello, What's up?, how it's going there?");
 	}
 
 }
